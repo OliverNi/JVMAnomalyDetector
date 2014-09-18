9f77da0237b90ef618f87c7bb54e5de4511709be
OliverNi <Oliver.Nilsson@hotmail.se>
2014-09-18 14:17:34 +0200

    Added Log

--- AnomalyDetector/AnomalyDetector.java
+++ AnomalyDetector/AnomalyDetector.java
@@ -1,18 +1,30 @@
 package AnomalyDetector;
 
+import Logs.Log;
+
+import java.util.ArrayList;
+
+
 import java.util.ArrayList;
 
 /**
  * Created by Oliver on 2014-09-10.
  */
 public class AnomalyDetector {
+
+    public Log getLog() {
+        return log;
+    }
+
+    private Log log;
     private ArrayList<JMXAgent> agents;
     public AnomalyDetector(){
         agents = new ArrayList<>();
     }
     public void connect(String hostName, int port){
-        agents.add(new JMXAgent(hostName, port,this));
+        agents.add(new JMXAgent(hostName, port, this));
     }
+
 
     /**
      * Testing purposes
