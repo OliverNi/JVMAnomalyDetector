2b986fe19eecaf385b29c566a946539c1678dbca
OliverNi <Oliver.Nilsson@hotmail.se>
2014-09-22 10:02:18 +0200

    Added disconnect and getConnections

--- AnomalyDetector/AnomalyDetector.java
+++ AnomalyDetector/AnomalyDetector.java
@@ -2,6 +2,7 @@ package AnomalyDetector;
 
 import Logs.Log;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
@@ -24,6 +25,29 @@ public class AnomalyDetector {
     public void connect(String hostName, int port){
         log = new Log();
         agents.add(new JMXAgent(hostName, port, this));
+    }
+
+    public boolean disconnect(String hostName, int port){
+        boolean disconnected = false;
+        for (JMXAgent a : agents) {
+            if (a.getHostName().equals(hostName) && a.getPort() == port) {
+                agents.remove(a);
+                disconnected = true;
+            }
+        }
+        return disconnected;
+    }
+
+    /**
+     * Get connection-names and port
+     * @return ArrayList<String> HOSTNAME:PORT
+     */
+    public ArrayList<String> getConnections(){
+        ArrayList<String> connections = new ArrayList<>();
+        for (JMXAgent a : agents){
+            connections.add(new String(a.getHostName() + ":" + a.getPort()));
+        }
+        return connections;
     }
 
 }
