06c16093b57dd149987de865cb9955b984be9dd6
OliverNi <Oliver.Nilsson@hotmail.se>
2014-09-24 08:41:08 +0200

    First commit

---
+++ AnomalyDetector/AnomalyReport.java
@@ -0,0 +1,14 @@
+package AnomalyDetector;
+
+/**
+ * Created by Oliver on 2014-09-24.
+ */
+
+/**
+ * Object created when an anomaly is found. This class is responsible for notifying the AnomalyDetector and
+ * Log to be stored away. An object of this class will contain information about the anomaly.
+ */
+public class AnomalyReport {
+
+
+}
