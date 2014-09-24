cd91d5a3458f161cba370e7559882f7fa2dbefff
Martin Olsson <tiggetagge@gmail.com>
2014-09-24 11:03:35 +0200

    Implemented 2 varieties each of methods : getMemoryStats() and getGarbageCollectionStats(). Implemented printSpecifiedTable() method for easier testing/debugging Changed variable name in AnalyzedGCData() for easier understanding of code.

--- Logs/Log.java
+++ Logs/Log.java
@@ -2,10 +2,7 @@ package Logs;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -39,6 +36,10 @@ public class Log implements  ILogging
         Connection connection = null;
         try
         {
+            //possibly secure the tables with a UNIQUE constraint to prevent duplicate rows.
+            // EXAMPLE:    CREATE TABLE a (i INT, j INT, UNIQUE(i, j) ON CONFLICT REPLACE);
+
+
 //            // create a database connection
             connection = DriverManager.getConnection("jdbc:sqlite:test.db");
             DB = connection.createStatement();
@@ -51,7 +52,7 @@ public class Log implements  ILogging
 
 //            DB.executeUpdate("create table AnalyzedGCData(analyzedId INTEGER PRIMARY KEY AUTOINCREMENT, avgCollected BIGINT, minCollected BIGINT, maxCollected BIGINT, minMemoryUsage BIGINT," +
 //                    "maxMemoryUsage BIGINT, avgMemoryUsage BIGINT, startMemoryUsage BIGINT, endMemoryUsage BIGINT,avgTimeBetweenGc BIGINT, " +
-//                    "minTimeBetweenGc BIGINT, maxTimeBetweenGc BIGINT, starttime BIGINT, endTime BIGINT, hostname VARCHAR(25), port INTEGER, trend VARCHAR(45), gcCount INTEGER)");
+//                    "minTimeBetweenGc BIGINT, maxTimeBetweenGc BIGINT, starttime BIGINT, endTime BIGINT, hostname VARCHAR(25), port INTEGER, trend VARCHAR(45), gcCount INTEGER, UNIQUE ) ");
 
             //   statement.executeUpdate("INSERT INTO MemLog(timestamp, usedMemory, hostname, port) VALUES(13371337, 10241024, '127.0.0.1', 3500)");
 
@@ -63,26 +64,19 @@ public class Log implements  ILogging
 //            statement.executeUpdate("insert into person values(1, 'leo')");
 //            statement.executeUpdate("insert into person values(2, 'yui')");
 
-//            AnalyzedGcStats temp = new AnalyzedGcStats();
-//            test.sendAnalyzedGCData("127.0.0.1",3800,temp);
+           // AnalyzedGcStats temp = new AnalyzedGcStats();
+         //   test.sendAnalyzedGCData("127.0.0.1",3800,temp);
 
             //this one gives duplicate rows when performed
          //  test.sendMemoryLog(1337,2233222, "127.0.0.1",3800);
-//
+            //
             //test.sendGarbageCollectionLog(0,0,0,0, "127.0.0.1", 3800);
            // DB.executeUpdate("DELETE FROM MemLog WHERE MemId = 4" );
-            ResultSet rs = DB.executeQuery("select * from MemLog");
+           // ResultSet rs = DB.executeQuery("select * from AnalyzedGCData");
 
-            while(rs.next())
-            {
-                // read the result set
 
-                System.out.println("MemId = " + rs.getString("MemId"));
-                System.out.println("timestamp = " + rs.getString("timestamp"));
-                System.out.println("usedMemory = " + rs.getString("usedMemory"));
-                System.out.println("hostname = " + rs.getString("hostname"));
-                System.out.println("port = " + rs.getString("port"));
-            }
+            printSpecifiedTable("AnalyzedGCData");
+
         }
         catch(SQLException e)
         {
@@ -103,35 +97,100 @@ public class Log implements  ILogging
 
     }
 
-    public long getGCTimeStamp() {
+    public static void printSpecifiedTable(String input) throws SQLException
+    {
+        try
+        {
+            ResultSet rs = DB.executeQuery("SELECT * FROM "+input);
+            while(rs.next())
+            {
+                if (input.equals("AnalyzedGCData"))
+                {
+                    System.out.println("analyzedId = " + rs.getString("analyzedId"));
+                    System.out.println("avgCollected = " + rs.getString("avgCollected"));
+                    System.out.println("minCollected = " + rs.getString("minCollected"));
+                    System.out.println("maxCollected = " + rs.getString("maxCollected"));
+                    System.out.println("minMemoryUsage = " + rs.getString("minMemoryUsage"));
+                    System.out.println("maxMemoryUsage = " + rs.getString("maxMemoryUsage"));
+                    System.out.println("avgMemoryUsage = " + rs.getString("avgMemoryUsage"));
+                    System.out.println("startMemoryUsage = " + rs.getString("startMemoryUsage"));
+                    System.out.println("endMemoryUsage = " + rs.getString("endMemoryUsage"));
+                    System.out.println("avgTimeBetweenGc = " + rs.getString("avgTimeBetweenGc"));
+                    System.out.println("minTimeBetweenGc = " + rs.getString("minTimeBetweenGc"));
+                    System.out.println("maxTimeBetweenGc = " + rs.getString("maxTimeBetweenGc"));
+                    System.out.println("startTime = " + rs.getString("startTime"));
+                    System.out.println("endTime = " + rs.getString("endTime"));
+                    System.out.println("hostname = " + rs.getString("hostname"));
+                    System.out.println("port = " + rs.getString("port"));
+                    System.out.println("trend = " + rs.getString("trend"));
+                    System.out.println("gcCount = " + rs.getString("gcCount"));
+                } else if (input.equals("MemLog"))
+                {
+                    System.out.println("MemId = " + rs.getString("MemId"));
+                    System.out.println("timestamp = " + rs.getString("timestamp"));
+                    System.out.println("usedMemory = " + rs.getString("usedMemory"));
+                    System.out.println("hostname = " + rs.getString("hostname"));
+                    System.out.println("port = " + rs.getString("port"));
+                }
+                else if(input.equals("GCLog"))
+                {
+                    System.out.println("gcId = " + rs.getString("gcId"));
+                    System.out.println("timestamp = " + rs.getString("timestamp"));
+                    System.out.println("memUsageAfter = " + rs.getString("memUsageBefore"));
+                    System.out.println("memUsageBefore = " + rs.getString("memUsageBefore"));
+                    System.out.println("GCCollectionTime = " + rs.getString("GCCollectionTime"));
+                    System.out.println("hostname = " + rs.getString("hostname"));
+                    System.out.println("port = " + rs.getString("port"));
+                }
+
+            }
+        }
+        catch(SQLException e)
+        {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    public long getGCTimeStamp()
+    {
         return GCTimeStamp;
     }
 
-    public long getGCmemoryUsageAfter() {
+    public long getGCmemoryUsageAfter()
+    {
         return GCmemoryUsageAfter;
     }
 
-    public long getGCmemoryUsageBefore() {
+    public long getGCmemoryUsageBefore()
+    {
         return GCmemoryUsageBefore;
     }
 
-    public long getMemoryUsed() {
+    public long getMemoryUsed()
+    {
         return memoryUsed;
     }
 
-    public long getTimeStamp() {
+    public long getTimeStamp()
+    {
         return timeStamp;
     }
 
-    public String getIp() {
+    public String getIp()
+    {
         return ip;
     }
 
-    public int getPort() {
+    public int getPort()
+    {
         return port;
     }
 
-    public long getGCTime() {
+    public long getGCTime()
+    {
         return GCTime;
     }
 
@@ -160,8 +219,6 @@ public class Log implements  ILogging
 
         return fetchTime;
     }
-
-
 
     public void setGCTime(long GCTime)
     {
@@ -203,7 +260,6 @@ public class Log implements  ILogging
         this.port = port;
     }
 
-
     //basic input values works
     @Override
     public void sendGarbageCollectionLog(long memoryUsedAfter, long memoryUsedBefore, long timestamp, long collectionTime, String hostname, int port)
@@ -225,7 +281,8 @@ public class Log implements  ILogging
         try
         {
             DB.executeUpdate("INSERT INTO  MemLog(timestamp,usedMemory,hostname,port) VALUES("+timestamp +","+memoryUsed+","+"'"+hostname+"'"+","+port+")");
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
             e.printStackTrace();
         }
     }
@@ -233,43 +290,199 @@ public class Log implements  ILogging
     @Override
     public Map<String, ArrayList<GcStats>> getGarbageCollectionStats(long startTime, long endTime)
     {
-        return null;
+        ArrayList<GcStats> getGCStats = new ArrayList<GcStats>();
+        GcStats GCstatistics = new GcStats();
+        String fetchHostNamePort = "";
+
+        try
+        {
+            ResultSet rs = DB.executeQuery("SELECT timestamp,memUsageAfter,memUsageBefore,hostname,port FROM GCLog WHERE startTime ="+startTime+" AND endTime ="+endTime);
+
+            fetchHostNamePort = rs.getString("hostname")+":"+rs.getString("port");
+
+            long timestamp = Long.parseLong(rs.getString("timestamp"));
+            GCstatistics.setTimeStamp(timestamp);
+
+            long  memUsageAfter = Long.parseLong(rs.getString("memUsageAfter"));
+            GCstatistics.setMemoryUsedAfter(memUsageAfter);
+
+            long memUsageBefore = Long.parseLong(rs.getString("memUsageBefore"));
+            GCstatistics.setMemoryUsedBefore(memUsageBefore);
+
+            long GCCollectionTime = Long.parseLong(rs.getString("GCCollectionTime"));
+            GCstatistics.setCollectionTime(GCCollectionTime);
+
+            getGCStats.add(GCstatistics);
+
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (NumberFormatException nfe)
+        {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+        }
+        HashMap<String, ArrayList<GcStats>> instanceOfGCStats = new HashMap<>();
+        instanceOfGCStats.put(fetchHostNamePort, getGCStats);
+        Map<String, ArrayList<GcStats>> fetch = instanceOfGCStats;
+
+        return fetch;
     }
 
     @Override
     public Map<String, ArrayList<GcStats>> getGarbageCollectionStats(long startTime, long endTime, ArrayList<String> processes)
     {
-        return null;
+        ArrayList<GcStats> getGCStats = new ArrayList<GcStats>();
+        GcStats GCstatistics = new GcStats();
+        String fetchHostNamePort = "";
+        String[] theSplit;
+
+        if (processes.toString().contains(":"))
+        {
+            String getPortHostname = processes.toString();
+            theSplit = getPortHostname.split("\\:");
+        } else {
+            throw new IllegalArgumentException("String " + processes.toString() + " does not contain :");
+        }
+        try
+        {
+            ResultSet rs = DB.executeQuery("SELECT timestamp,memUsageAfter,memUsageBefore,hostname,port FROM GCLog " +
+                "WHERE startTime ="+startTime+" AND endTime ="+endTime +" AND port = "+theSplit[1]+" AND hostname = "+theSplit[0]);
+
+            fetchHostNamePort = rs.getString("hostname")+":"+rs.getString("port");
+
+            long timestamp = Long.parseLong(rs.getString("timestamp"));
+            GCstatistics.setTimeStamp(timestamp);
+
+            long  memUsageAfter = Long.parseLong(rs.getString("memUsageAfter"));
+            GCstatistics.setMemoryUsedAfter(memUsageAfter);
+
+            long memUsageBefore = Long.parseLong(rs.getString("memUsageBefore"));
+            GCstatistics.setMemoryUsedBefore(memUsageBefore);
+
+            long GCCollectionTime = Long.parseLong(rs.getString("GCCollectionTime"));
+            GCstatistics.setCollectionTime(GCCollectionTime);
+
+            getGCStats.add(GCstatistics);
+
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (NumberFormatException nfe)
+        {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+        }
+        HashMap<String, ArrayList<GcStats>> instanceOfGCStats = new HashMap<>();
+        instanceOfGCStats.put(fetchHostNamePort,getGCStats);
+        Map<String, ArrayList<GcStats>> fetch = instanceOfGCStats;
+
+        return fetch;
     }
 
     @Override
     public Map<String, ArrayList<MemoryStats>> getMemoryStats(long startTime, long endTime)
     {
-        return null;
+        ArrayList<MemoryStats> getMemStats = new ArrayList<MemoryStats>();
+        MemoryStats memstats = new MemoryStats();
+        String fetchHostNamePort = "";
+
+        try
+        {
+            ResultSet rs = DB.executeQuery("SELECT timestamp,usedMemory,hostname,port FROM GCLog " +
+                    "WHERE startTime ="+startTime+" AND endTime ="+endTime);
+
+            fetchHostNamePort = rs.getString("hostname")+":"+rs.getString("port");
+
+            long timestamp = Long.parseLong(rs.getString("timestamp"));
+            memstats.setTimeStamp(timestamp);
+
+            long  usedMemory = Long.parseLong(rs.getString("usedMemory"));
+            memstats.setMemoryUsed(usedMemory);
+
+            getMemStats.add(memstats);
+
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (NumberFormatException nfe)
+        {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+        }
+        HashMap<String, ArrayList<MemoryStats>> instanceOfMemStats = new HashMap<>();
+        instanceOfMemStats.put(fetchHostNamePort,getMemStats);
+        Map<String, ArrayList<MemoryStats>> fetch = instanceOfMemStats;
+
+        return fetch;
     }
 
     @Override
     public Map<String, ArrayList<MemoryStats>> getMemoryStats(long startTime, long endTime, ArrayList<String> processes)
     {
-        return null;
+        ArrayList<MemoryStats> getMemStats = new ArrayList<MemoryStats>();
+        MemoryStats memstats = new MemoryStats();
+        String fetchHostNamePort = "";
+        String[] theSplit;
+
+        if (processes.toString().contains(":"))
+        {
+            String getPortHostname = processes.toString();
+            theSplit = getPortHostname.split("\\:");
+        } else {
+            throw new IllegalArgumentException("String " + processes.toString() + " does not contain :");
+        }
+
+        try
+        {
+            ResultSet rs = DB.executeQuery("SELECT timestamp,usedMemory,hostname,port FROM GCLog " +
+                    "WHERE startTime ="+startTime+" AND endTime ="+endTime+" AND hostname = "+theSplit[0]+" AND port = "+theSplit[1]);
+
+            fetchHostNamePort = rs.getString("hostname")+":"+rs.getString("port");
+
+            long timestamp = Long.parseLong(rs.getString("timestamp"));
+            memstats.setTimeStamp(timestamp);
+
+            long  usedMemory = Long.parseLong(rs.getString("usedMemory"));
+            memstats.setMemoryUsed(usedMemory);
+
+            getMemStats.add(memstats);
+
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (NumberFormatException nfe)
+        {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+        }
+        HashMap<String, ArrayList<MemoryStats>> instanceOfMemStats = new HashMap<>();
+        instanceOfMemStats.put(fetchHostNamePort,getMemStats);
+        Map<String, ArrayList<MemoryStats>> fetch = instanceOfMemStats;
+
+        return fetch;
     }
 
 
-    //input values works
+    //input values works, however an update is needed for the trend column, what value to implement when project member is finished with his new concept
     @Override
-    public void sendAnalyzedGCData(String hostName, int port, AnalyzedGcStats analyzedDailyGcStats)
+    public void sendAnalyzedGCData(String hostName, int port, GcReport analyzedDailyGcStats)
     {
         try
         {
-            String fetch = "INSERT INTO  AnalyzedGCData(avgCollected, minCollected,maxCollected,minMemoryUsage,maxMemoryUsage," +
+            String input = "INSERT INTO  AnalyzedGCData(avgCollected, minCollected,maxCollected,minMemoryUsage,maxMemoryUsage," +
                     "avgMemoryUsage,startMemoryUsage,endMemoryUsage,avgTimeBetweenGc,minTimeBetweenGc,maxTimeBetweenGc,starttime,endtime,hostname,port,trend,gcCount) " +
                     "VALUES("+ analyzedDailyGcStats.getAvgCollected()+","+analyzedDailyGcStats.getMinCollected()+","+analyzedDailyGcStats.getMaxCollected()+","+analyzedDailyGcStats.getMinMemoryUsage()+","
                     +analyzedDailyGcStats.getMaxMemoryUsage()+","+analyzedDailyGcStats.getAvgMemoryUsage()+","+
                     analyzedDailyGcStats.getStartMemoryUsage()+","+analyzedDailyGcStats.getEndMemoryUsage()+","+analyzedDailyGcStats.getAvgTimeBetweenGc()+","+
-            analyzedDailyGcStats.getMinTimeBetweenGc()+","+analyzedDailyGcStats.getMaxTimeBetweenGc()+","+analyzedDailyGcStats.getStartTime()+","+analyzedDailyGcStats.getEndTime()+",'"+hostName+"',"+port+",'"+analyzedDailyGcStats.getTrend()+"',"
+            analyzedDailyGcStats.getMinTimeBetweenGc()+","+analyzedDailyGcStats.getMaxTimeBetweenGc()+","+analyzedDailyGcStats.getStartTime()+","+analyzedDailyGcStats.getEndTime()+",'"+hostName+"',"+port+",'TEMP STABLE TREND VALUE',"
                     +analyzedDailyGcStats.getGcCount()+")";
-            System.out.println(fetch);
-            DB.executeUpdate(fetch);
+            System.out.println(input);
+            DB.executeUpdate(input);
         } catch (SQLException e) {
             e.printStackTrace();
         }
