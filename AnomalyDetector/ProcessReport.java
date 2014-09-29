90119254a472227e1ac9d9f45b62445443a67b6e
Martin Olsson <tiggetagge@gmail.com>
2014-09-29 11:26:10 +0200

    created standard percentage warning variable and connected it and the default GC time warning in the constructor

--- AnomalyDetector/ProcessReport.java
+++ AnomalyDetector/ProcessReport.java
@@ -17,18 +17,24 @@ public class ProcessReport {
         EXCESSIVE_GC_SCAN,
         OK
     }
-    public static final long DEFAULT_TIME_BETWEEN_GC_WARNING = 5000;
+    // default value in milliseconds (20min) corresponding to a warning
+    public static final long DEFAULT_TIME_BETWEEN_GC_WARNING = 1200000;
+    //default value of a percentage increase corresponding to a warning
+    public static final double DEFAULT_PERCENTAGE_INC_IN_MEM_USE_WARNING = 0.1;
     public static long TIME_BETWEEN_GC_WARNING;
+    public static double PERCENTAGE_INC_IN_MEM_USE_WARNING;
+
+    private int uptimeInDays;
     private String hostName;
     private int port;
     private Status status;
 
     //GC
     //Usage after first recorded GC
-    private int usageAfterFirstGc;
+    private long usageAfterFirstGc;
 
     //Usage after last recorded GC
-    private int usageAfterLastGc;
+    private long usageAfterLastGc;
 
     //Difference (percentage) in average memory usage
     private double dailyAvgMemUsageDif;
@@ -54,6 +60,7 @@ public class ProcessReport {
 
     public ProcessReport(String hostName, int port){
         TIME_BETWEEN_GC_WARNING = DEFAULT_TIME_BETWEEN_GC_WARNING;
+        PERCENTAGE_INC_IN_MEM_USE_WARNING = DEFAULT_PERCENTAGE_INC_IN_MEM_USE_WARNING;
     }
 
     public ProcessReport(){
