cbd8119a79d85d8fe61c156ffd14f422052465e8
Martin Olsson <tiggetagge@gmail.com>
2014-09-29 15:20:05 +0200

    added variables that  may  be useful for  anomalyreport and creating a constructor with the input parameters.

--- AnomalyDetector/AnomalyReport.java
+++ AnomalyDetector/AnomalyReport.java
@@ -8,12 +8,34 @@ package AnomalyDetector;
  * Object created when an anomaly is found. This class is responsible for notifying the AnomalyDetector and
  * Log to be stored away. An object of this class will contain information about the anomaly.
  */
-public class AnomalyReport {
-    public enum Anomaly{
-        EXCESSIVE_GC_SCAN,
-        OVER_TIME_INCREASING_MEMORY_USAGE,
-        RAPIDLY_INCREASING_MEMORY_USAGE
+public class AnomalyReport
+{
+    private String hostname;
+    private int port;
+    private long timePeriod;
+    private String errorMessage;
+    private String errorAccuracy;
+    private long anomalyStartTime;
+
+    public AnomalyReport()
+    {
+        hostname = "";
+        port = 0;
+        timePeriod = 0;
+        errorMessage  = "none";
+        errorAccuracy = "none";
+        anomalyStartTime = 0;
     }
-    private Anomaly type;
+
+    public AnomalyReport(long anomalyStartTime, long timePeriod, String errorMessage, String errorAccuracy, String hostname, int port)
+    {
+        this.anomalyStartTime = anomalyStartTime;
+        this.timePeriod = timePeriod;
+        this.errorMessage = errorMessage;
+        this.errorAccuracy = errorAccuracy;
+        this.hostname = hostname;
+        this.port = port;
+    }
+
 
 }
