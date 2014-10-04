27a8b2bfc3b68ed415877c99f837d09fd2543c97
Oliver Nilsson <Oliver.Nilsson@hotmail.se>
2014-10-04 15:56:22 +0200

    Implemented addReport (DAILY)

--- AnomalyDetector/ProcessReport.java
+++ AnomalyDetector/ProcessReport.java
@@ -54,11 +54,13 @@ public class ProcessReport
     private double monthlyMinMemUsageDif;
     private double weeklyMinMemUsageDif;
 
+    //@TODO Possibly remove, not sure how it would be used to find memory leaks. Should instead focus on minimum value
     //How many times have the memory usage increased compared to the previous day/week/month
     private int dailyIncreaseCount;
     private int weeklyIncreaseCount;
     private int monthlyIncreaseCount;
 
+    //@TODO Possibly remove, not sure how it would be used to find memory leaks. Should instead focus on minimum value
     //How many times have the memory usage decreased compared to the previous day/week/month
     private int dailyDecreaseCount;
     private int weeklyDecreaseCount;
@@ -150,7 +152,25 @@ public class ProcessReport
     }
 
     public void addReport(AnalyzedGcReport report){
+        switch(report.getType()){
+            case HOURLY:
 
+                break;
+            case DAILY:
+                if (report.getAvgMinMemoryUsageDif() < dailyMinMemUsageDif)
+                    dailyMinMemUsageDif = report.getAvgMinMemoryUsageDif();
+                if (report.getAvgMemoryUsageDif() > 1)
+                    dailyIncreaseCount++;
+                else if (report.getAvgMemoryUsageDif() < 1)
+                    dailyDecreaseCount++;
+                dailySumMemUsageDif += report.getAvgMemoryUsageDif();
+                dailyReportCount++;
+                break;
+            case WEEKLY:
+                break;
+            case MONTHLY:
+                break;
+        }
     }
 
     private void createAnomalyReport(){
