54cc2183cd8e48a42b73fd1240cf38094f81a5d5
Oliver Nilsson <Oliver.Nilsson@hotmail.se>
2014-10-17 11:44:01 +0200

    GUI: Added functionality for getting today's / this week's / this month's GcStats

--- GUI/Models/GcStatsModel.java
+++ GUI/Models/GcStatsModel.java
@@ -6,6 +6,7 @@ import Logs.Log;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Oliver on 2014-10-15.
@@ -18,21 +19,61 @@ public class GcStatsModel extends Model<GcStatsResponse>{
     }
 
     public void getListItems(String host, int port, String period){
-        ArrayList<GcStats> stats = new ArrayList<>();
+        ArrayList<GcStats> result = new ArrayList<>();
+        Calendar cal = Calendar.getInstance();
         switch(period){
             case "All":
-                stats = log.getGarbageCollectionStats(0L, Calendar.getInstance().getTimeInMillis(), host, port);
+                result = log.getGarbageCollectionStats(0L, Calendar.getInstance().getTimeInMillis(), host, port);
                 break;
-            case "Today":
+            case "Today": {
+                cal.set(Calendar.HOUR, 0);
+                cal.set(Calendar.SECOND, 0);
+                cal.set(Calendar.MINUTE, 0);
+                Date todayStart = cal.getTime();
+                cal.set(Calendar.HOUR, 23);
+                cal.set(Calendar.SECOND, 59);
+                cal.set(Calendar.MINUTE, 59);
+                Date todayEnd = cal.getTime();
+
+                result = log.getGarbageCollectionStats(todayStart.getTime(), todayEnd.getTime(), host, port);
                 break;
-            case "This week":
+            }
+            case "This week": {
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                cal.set(Calendar.HOUR, 0);
+                cal.set(Calendar.SECOND, 0);
+                cal.set(Calendar.MINUTE, 0);
+                Date weekStart = cal.getTime();
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                cal.set(Calendar.HOUR, 23);
+                cal.set(Calendar.SECOND, 59);
+                cal.set(Calendar.MINUTE, 59);
+                Date weekEnd = cal.getTime();
+
+                result = log.getGarbageCollectionStats(weekStart.getTime(), weekEnd.getTime(), host, port);
                 break;
-            case "This month":
+            }
+            case "This month": {
+                cal.set(Calendar.DAY_OF_MONTH, 1);
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                cal.set(Calendar.HOUR, 0);
+                cal.set(Calendar.SECOND, 0);
+                cal.set(Calendar.MINUTE, 0);
+                Date monthStart = cal.getTime();
+                cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                cal.set(Calendar.HOUR, 23);
+                cal.set(Calendar.SECOND, 59);
+                cal.set(Calendar.MINUTE, 59);
+                Date monthEnd = cal.getTime();
+
+                result = log.getGarbageCollectionStats(monthStart.getTime(), monthEnd.getTime(), host, port);
                 break;
+            }
         }
-        if (stats != null) {
+        if (result != null) {
             for (GcStatsResponse o : this.getObservers())
-                o.searchResult(stats);
+                o.searchResult(result);
         }
     }
 }
