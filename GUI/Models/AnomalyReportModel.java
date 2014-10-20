0a8e8e06de570be4771d0deb04e5d772d8cf6801
OliverNi <Oliver.Nilsson@hotmail.se>
2014-10-20 14:02:15 +0200

    Changed time (Added AM_PM)

--- GUI/Models/AnomalyReportModel.java
+++ GUI/Models/AnomalyReportModel.java
@@ -21,25 +21,30 @@ public class AnomalyReportModel extends Model<AnomalyReportResponse> {
                 result = log.getAnomalyReports(host, port);
                 break;
             case "Today": {
+                cal.set(Calendar.AM_PM, Calendar.AM);
                 cal.set(Calendar.HOUR, 0);
-                cal.set(Calendar.SECOND, 0);
+                cal.set(Calendar.SECOND, 1);
                 cal.set(Calendar.MINUTE, 0);
                 Date start = cal.getTime();
+                cal.set(Calendar.AM_PM, Calendar.PM);
                 cal.set(Calendar.HOUR, 23);
                 cal.set(Calendar.SECOND, 59);
                 cal.set(Calendar.MINUTE, 59);
                 Date end = cal.getTime();
-
+                System.out.println(start.toString());
                 result = log.getAnomalyReports(start.getTime(), end.getTime(), host, port);
                 break;
             }
             case "This week": {
                 cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                cal.set(Calendar.AM_PM, Calendar.AM);
                 cal.set(Calendar.HOUR, 0);
-                cal.set(Calendar.SECOND, 0);
+                cal.set(Calendar.SECOND, 1);
                 cal.set(Calendar.MINUTE, 0);
                 Date start = cal.getTime();
+                System.out.println(start.toString());
                 cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                cal.set(Calendar.AM_PM, Calendar.PM);
                 cal.set(Calendar.HOUR, 23);
                 cal.set(Calendar.SECOND, 59);
                 cal.set(Calendar.MINUTE, 59);
@@ -51,12 +56,14 @@ public class AnomalyReportModel extends Model<AnomalyReportResponse> {
             case "This month": {
                 cal.set(Calendar.DAY_OF_MONTH, 1);
                 cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                cal.set(Calendar.AM_PM, Calendar.AM);
                 cal.set(Calendar.HOUR, 0);
                 cal.set(Calendar.SECOND, 0);
                 cal.set(Calendar.MINUTE, 0);
                 Date start = cal.getTime();
                 cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
                 cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                cal.set(Calendar.AM_PM, Calendar.PM);
                 cal.set(Calendar.HOUR, 23);
                 cal.set(Calendar.SECOND, 59);
                 cal.set(Calendar.MINUTE, 59);
